16df80d6e7d15d878b7d0f1701a2b8a15698f102
Jozhuas <20230102@ricaldone.edu.sv>
2025-08-22 10:36:33 -0600

    Archivo

---
+++ src/main/java/JoshuaMarinero_20230102/JoshuaMarinero_20230102/ProyectoModuloJoshuaMarinero20230102Application.java
@@ -0,0 +1,13 @@
+package JoshuaMarinero_20230102.JoshuaMarinero_20230102;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoModuloJoshuaMarinero20230102Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoModuloJoshuaMarinero20230102Application.class, args);
+	}
+
+}
---
+++ src/test/java/JoshuaMarinero_20230102/JoshuaMarinero_20230102/ProyectoModuloJoshuaMarinero20230102ApplicationTests.java
@@ -0,0 +1,13 @@
+package JoshuaMarinero_20230102.JoshuaMarinero_20230102;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoModuloJoshuaMarinero20230102ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
